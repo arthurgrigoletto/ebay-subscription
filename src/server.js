2e5c1fd68b40651c9835e72118474892d357456a
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2020-02-09 20:31:50 -0300

    Connected with mongo

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 import app from './app';
+import mongoConfig from './config/mongo';
+
+mongoose.connect(
+  `mongodb://${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.database}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 const port = process.env.PORT || 3333;
 
