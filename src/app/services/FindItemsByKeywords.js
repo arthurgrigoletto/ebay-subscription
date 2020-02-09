93c9e41a0fa42cb1d0e6dac1b9d98cff0933556d
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2020-02-09 20:26:29 -0300

    Created service to get ebay products

---
+++ src/app/services/FindItemsByKeywords.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+import ebayConfig from '../../config/ebay';
+
+class FindItemsByKeywords {
+  async run(keywords) {
+    const { data } = await axios.get(
+      'https://svcs.sandbox.ebay.com/services/search/FindingService/v1',
+      {
+        params: {
+          'SECURITY-APPNAME': ebayConfig.clientID,
+          'RESPONSE-DATA-FORMAT': 'JSON',
+          keywords,
+          'OPERATION-NAME': 'findItemsByKeywords',
+          'paginationInput.entriesPerPage': 3,
+        },
+      }
+    );
+
+    const { searchResult } = data.findItemsByKeywordsResponse.find(item =>
+      item.ack.includes('Success')
+    );
+
+    const { item: items } = searchResult.find(result => result['@count'] >= 3);
+
+    const products = items.map(item => ({
+      id: item.itemId[0],
+      title: item.title[0],
+      url: item.viewItemURL[0],
+      // eslint-disable-next-line no-underscore-dangle
+      price: item.sellingStatus[0].currentPrice[0].__value__,
+    }));
+
+    return products;
+  }
+}
+
+export default new FindItemsByKeywords();
---
+++ src/config/ebay.js
@@ -0,0 +1,3 @@
+export default {
+  clientID: process.env.EBAY_CLIENT_ID,
+};
