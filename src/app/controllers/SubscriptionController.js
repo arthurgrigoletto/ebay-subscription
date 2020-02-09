d7710076bfa2137ee33e1b865c6bdb810487031f
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2020-02-09 20:24:54 -0300

    Initiated Subscription Controller

---
+++ src/app/controllers/SubscriptionController.js
@@ -0,0 +1,9 @@
+class SubscriptionController {
+  // async index(req, res) {}
+  // async show(req, res) {}
+  // async store(req, res) {}
+  // async update(req, res) {}
+  // async destroy(req, res) {}
+}
+
+export default new SubscriptionController();
